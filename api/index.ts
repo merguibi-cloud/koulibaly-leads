c16d6cac4c036b4ed74ca2c8662ffa85754bfb29
merguibi-cloud <m.erguibi@elite-society.fr>
2026-04-15 14:14:33 +0100

    cleaner architecture

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,9 @@
 import "dotenv/config";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "../server/_core/oauth";
 import { appRouter } from "../server/routers";
 import { createContext } from "../server/_core/context";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -24,12 +19,5 @@ app.use(
     createContext,
   })
 );
-
-// Serve static files built by vite
-const staticPath = path.resolve(__dirname, "../dist/public");
-app.use(express.static(staticPath));
-app.use("*", (_req, res) => {
-  res.sendFile(path.resolve(staticPath, "index.html"));
-});
 
 export default app;
