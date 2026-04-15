2c5ba91abe7526fa9cca1b948e2390e2a88cb3dc
merguibi-cloud <m.erguibi@elite-society.fr>
2026-04-15 18:17:20 +0100

    fix: compile serverless entry with esbuild to avoid path alias issues

--- server/serverless.ts
+++ server/serverless.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "../server/_core/oauth";
-import { appRouter } from "../server/routers";
-import { createContext } from "../server/_core/context";
+import { registerOAuthRoutes } from "./_core/oauth";
+import { appRouter } from "./routers";
+import { createContext } from "./_core/context";
 
 const app = express();
 
